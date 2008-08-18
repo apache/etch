ec06c4950779d750319f6af3c70075cdd7eeaa27
Robert Scott Comer <sccomer@apache.org>
2008-08-18 03:39:54 +0000

    new files to model compiler output.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/etch/branches/sccomer-compiler-1@712751 13f79535-47bb-0310-9956-ffa450edef68

---
+++ compiler/src/main/java/etch/compiler/FileOutput.java
@@ -0,0 +1,72 @@
+package etch.compiler;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FileOutput implements Output
+{
+	public FileOutput()
+	{
+		this( null );
+	}
+	
+	public FileOutput( String pn )
+	{
+		this.pn = pn;
+	}
+	
+	private final String pn;
+	
+	public void report()
+	{
+		report( "" );
+	}
+	
+	private void report( String indent )
+	{
+		if (pn != null)
+			System.out.printf( "%spackage %s", indent, pn );
+		else
+			System.out.printf( "%soutput", indent );
+		System.out.println();
+		
+		String sindent = indent+"  ";
+		
+		for (Map.Entry<String, ByteArrayOutputStream> me: files.entrySet())
+			report( sindent, me.getKey(), me.getValue() );
+		
+		for (FileOutput p: packages.values())
+			p.report( sindent );
+	}
+
+	private void report( String indent, String fn, ByteArrayOutputStream value )
+	{
+		System.out.printf( "%sfile %s %d", indent, fn, value.size() );
+		System.out.println();
+	}
+
+	public OutputStream newOutputStream( String fn )
+	{
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		files.put( fn, baos );
+		return baos;
+	}
+	
+	private final Map<String, ByteArrayOutputStream> files =
+		new HashMap<String, ByteArrayOutputStream>();
+
+	public Output newPackage( String pn )
+	{
+		FileOutput p = packages.get( pn );
+		if (p == null)
+		{
+			p = new FileOutput( pn );
+			packages.put( pn, p );
+		}
+		return p;
+	}
+	
+	private final Map<String, FileOutput> packages = new HashMap<String, FileOutput>();
+}
