2a76851d8eba07f7f36edea59d4c282b72c69be5
dixson3 <dev-null@apache.org>
2008-09-02 16:16:17 +0000

    unset ETCH_HOME
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/etch/trunk@712789 13f79535-47bb-0310-9956-ffa450edef68

--- Bamboo.bat
+++ Bamboo.bat
@@ -4,6 +4,7 @@
 
 :: Run our env setup script
 call %TOOLS_DIR%\Environment.bat -qnc -p ETCH
+set ETCH_HOME=
 
 :: Hack to get DOTNET defined. Environment.bat should do it.
 set DOTNET_HOME=C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727
