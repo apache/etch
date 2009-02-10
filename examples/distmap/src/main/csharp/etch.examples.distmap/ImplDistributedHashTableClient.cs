c65b22535017c9a6fc1894babab19d2610530ea2
Robert Scott Comer <sccomer@apache.org>
2009-02-10 22:53:01 +0000

    fix for ETCH-45: put java header comments in order per apache standard
    some csharp headers adjusted, but they are not yet the proper text.
    
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/etch/trunk@743147 13f79535-47bb-0310-9956-ffa450edef68

--- examples/distmap/src/main/csharp/etch.examples.distmap/ImplDistributedHashTableClient.cs
+++ examples/distmap/src/main/csharp/etch.examples.distmap/ImplDistributedHashTableClient.cs
@@ -1,23 +1,19 @@
-/* $Id$
- *
- * Copyright 2007-2008 Cisco Systems Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy
- * of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations
- * under the License.
- */
+// $Id$
+//
+// Copyright 2007-2008 Cisco Systems Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License. You may obtain a copy
+// of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+// License for the specific language governing permissions and limitations
+// under the License.
 
-
-///<summary>Your custom implementation of BaseDistributedHashTableClient. Add methods here to provide
-///implementation of messages from the server. </summary>
 namespace org.apache.etch.examples.distmap
 {
 	///<summary>Implementation for ImplDistributedHashTableClient</summary>
