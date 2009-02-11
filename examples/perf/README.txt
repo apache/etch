c3e53b3042775b39ad812b186e941f9051e199fe
James S. Dixson III <dixson@apache.org>
2009-02-11 16:57:28 +0000

    ETCH-49 - merge changes from 1.0.2 into trunk (specifcally license updates)
    git-svn-id: https://svn.apache.org/repos/asf/incubator/etch/trunk@743398 13f79535-47bb-0310-9956-ffa450edef68

--- examples/perf/README.txt
+++ examples/perf/README.txt
@@ -1,10 +1,28 @@
+To get started with the example you will need jdk 1.5.0_x,
+where x is pretty recent (>= 15), or any release of jdk 1.6.
+You will also need ant 1.7.0, with ant .net plugin if you
+want to build csharp. You will also need microsoft .net
+sdk 2.0 for csharp builds.
 
 To build:
 
-> ant build-all
+> ant
 
-To Run:
+This will run the default target which will build both java
+and csharp programs. You can also use build-java or build-csharp
+targets to build only java or only csharp.
 
-> cd target/bin
-> java -jar perflistener.jar &
-> java -jar perfclient.jar
+To run java performance example, open a cmd window and run these
+commands:
+
+> cd target\bin
+> start java -cp perf.jar etch.examples.perf.MainPerfListener
+> start java -cp perf.jar etch.examples.perf.MainPerfClient
+
+To run csharp example:
+
+> cd target\bin
+> start perfListener.exe
+> start perfClient.exe
+
+You can mix and match the various clients and listeners.
--- scripts/antsetup.bat
+++ scripts/antsetup.bat
@@ -1,14 +1,25 @@
-# example setup script for ant build environment.
-# optional stuff may be omitted by prefixing every
-# line between the brackets with rem.
-
-# look for additional information in build.dependencies.
+@rem  Licensed to the Apache Software Foundation (ASF) under one   *
+@rem  or more contributor license agreements.  See the NOTICE file *
+@rem  distributed with this work for additional information        *
+@rem  regarding copyright ownership.  The ASF licenses this file   *
+@rem  to you under the Apache License, Version 2.0 (the            *
+@rem  "License"); you may not use this file except in compliance   *
+@rem  with the License.  You may obtain a copy of the License at   *
+@rem                                                               *
+@rem    http://www.apache.org/licenses/LICENSE-2.0                 *
+@rem                                                               *
+@rem  Unless required by applicable law or agreed to in writing,   *
+@rem  software distributed under the License is distributed on an  *
+@rem  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+@rem  KIND, either express or implied.  See the License for the    *
+@rem  specific language governing permissions and limitations      *
+@rem  under the License.                                           *
 
 rem -- java builds --
 
 set TOOLS_DIR=C:\wks\tools
 set ANT_HOME=%TOOLS_DIR%\apache-ant\1.7.0
-ren set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_06
+rem set JAVA_HOME=C:\Program Files\Java\jdk1.6.0_06
 rem optional
 rem set CLOVER_HOME=%TOOLS_DIR%\clover-ant\2.3.2
 rem /optional
