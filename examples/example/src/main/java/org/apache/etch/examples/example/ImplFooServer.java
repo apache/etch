cd8f3fd768be4e0292db57d5a6ccdc77279a44aa
Robert Scott Comer <sccomer@apache.org>
2009-02-27 13:28:04 +0000

    updated demos.
    git-svn-id: https://svn.apache.org/repos/asf/incubator/etch/branches/dynamo@748509 13f79535-47bb-0310-9956-ffa450edef68

--- examples/example/src/main/java/org/apache/etch/examples/example/ImplFooServer.java
+++ examples/example/src/main/java/org/apache/etch/examples/example/ImplFooServer.java
@@ -7,6 +7,11 @@
 
 package org.apache.etch.examples.example;
 
+import org.apache.etch.bindings.java.msg.Message;
+import org.apache.etch.bindings.java.msg.Type;
+import org.apache.etch.bindings.java.support.RetryMessageException;
+import org.apache.etch.bindings.java.transport.UnwantedMessage;
+
 /**
  * Your custom implementation of BaseFooServer. Add methods here to provide
  * implementations of messages from the client.
@@ -28,12 +33,36 @@ public class ImplFooServer extends BaseFooServer
 	 * A connection to the client session. Use this to send a
 	 * message to the client.
 	 */
-	@SuppressWarnings( "unused" )
 	private final RemoteFooClient client;
 	
 	@Override
 	public Integer foo( String msg )
 	{
 		return msg.hashCode();
+	}
+	
+	@Override
+	public void _sessionNotify( Object event ) throws Exception
+	{
+		if (event instanceof UnwantedMessage)
+		{
+			Message msg = ((UnwantedMessage) event).msg;
+			Type type = msg.type();
+			System.out.println( "unknown msg type "+type );
+			
+			String uri = (String) client._transportQuery( "uri" );
+			
+			// try to load ValueFactoryExample
+
+			ValueFactoryExample nvf = new ValueFactoryExample( uri );
+			if (nvf.getType( type.getId() ) != null)
+			{
+				System.out.println( "loading "+ValueFactoryExample.class.getName() );
+				ExampleHelper.addServer( nvf, client );
+				throw new RetryMessageException();
+			}
+			
+			// no other service to try, fall through...
+		}
 	}
 }
