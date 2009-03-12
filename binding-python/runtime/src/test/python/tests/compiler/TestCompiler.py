70c854fd7a6dd8c56b685acab7388d5189c82add
James S. Dixson III <dixson@apache.org>
2009-03-12 00:26:48 +0000

    unit test package refactor
    git-svn-id: https://svn.eu.apache.org/repos/asf/incubator/etch/trunk@752718 13f79535-47bb-0310-9956-ffa450edef68

---
+++ binding-python/runtime/src/test/python/tests/compiler/TestCompiler.py
@@ -0,0 +1,13 @@
+import unittest
+
+class Test_EtchCompiler(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test1(self):
+        # placeholder
+        pass
+
+if __name__=='__main__':
+    unittest.main()
---
+++ binding-python/runtime/src/test/python/tests/util/TestUtil.py
@@ -0,0 +1,13 @@
+import unittest
+
+class Test_EtchUtil(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test1(self):
+        # placeholder
+        pass
+
+if __name__=='__main__':
+    unittest.main()
