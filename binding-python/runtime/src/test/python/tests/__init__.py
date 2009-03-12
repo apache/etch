c6dd4b45b4d8c6a23689da6a15c0792ae8520c33
James S. Dixson III <dixson@apache.org>
2009-03-12 00:26:48 +0000

    unit test package refactor
    git-svn-id: https://svn.apache.org/repos/asf/incubator/etch/trunk@752718 13f79535-47bb-0310-9956-ffa450edef68

--- binding-python/runtime/src/test/python/tests/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import binding
-from . import support
-from . import transport
-from . import util
-
-#from . import util
-#from . import python
-#from . import compiler
---
+++ binding-python/runtime/src/test/python/tests/binding/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+
+from . import msg
+from . import support
+from . import transport
+from . import util
