2c547ccbf19a88883fdf0c58f019bf377167ae67
Michael Fitzner <fitzner@apache.org>
2010-11-15 16:03:36 +0000

    [ETCH-116] Change \scripts\antsetup.bat so external ENV variables are also handled
    git-svn-id: https://svn.apache.org/repos/asf/incubator/etch/trunk@1035328 13f79535-47bb-0310-9956-ffa450edef68

--- scripts/antsetup.bat
+++ scripts/antsetup.bat
@@ -16,15 +16,31 @@
 @rem  under the License.                                           *
 @echo off
 
+@rem ETCH_EXTERNAL_DEPENDS settings
 IF "%ETCH_EXTERNAL_DEPENDS%" == "" (
 set ETCH_EXTERNAL_DEPENDS=C:\etch\external
 )
-
 set ANT_HOME=%ETCH_EXTERNAL_DEPENDS%\apache-ant\1.7.0
-set JAVA_HOME=c:\Program Files (x86)\Java\jdk1.6.0_18\
+
+@rem JAVA_HOME settings
+IF "%JAVA_HOME%" == "" (
+set JAVA_HOME="c:\Program Files (x86)\Java\jdk1.6.0_18\"
+)
+
+@rem NUNIT_HOME settings
+IF "%NUNIT_HOME%" == "" (
 set NUNIT_HOME=%ETCH_EXTERNAL_DEPENDS%\NUnit\2.4.7
+)
 
 rem uncomment this line / unset this variable if you don't want to build csharp
 set DOTNET_HOME=C:\WINDOWS\Microsoft.NET\Framework\v2.0.50727
 
 set PATH=%ANT_HOME%\bin;%JAVA_HOME%\bin;%DOTNET_HOME%;%NUNIT_HOME%\bin;%PATH%
+
+@echo ==================================================
+@echo Etch Configuration
+@echo ==================================================
+@echo ETCH_EXTERNAL_DEPENDS: %ETCH_EXTERNAL_DEPENDS% 
+@echo ANT_HOME: %ANT_HOME%
+@echo JAVA_HOME: %JAVA_HOME%
+@echo ==================================================
