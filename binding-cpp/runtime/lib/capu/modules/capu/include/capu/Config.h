b7450ba7c3f8c5ca4aa1d37ec49a21cd690b2cb7
Michael Fitzner <fitzner@apache.org>
2011-08-22 13:27:56 +0000

    ETCH-132
    Added small change in configuration header
    
    Change-Id: I7b61d5489d8ec950d5bb147e2e95f59700603f81
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/etch/trunk@1160264 13f79535-47bb-0310-9956-ffa450edef68

--- binding-cpp/runtime/lib/capu/modules/capu/include/capu/Config.h
+++ binding-cpp/runtime/lib/capu/modules/capu/include/capu/Config.h
@@ -23,16 +23,20 @@
 #include "stdlib.h"
 #include "string.h"
 
+
 namespace capu
 {
-    #ifdef _WIN32
+   #ifdef _WIN32
     typedef char int8_t;
     typedef short int16_t;
     typedef int int32_t;
     typedef signed __int64 int64_t;
     #else //other OS should be C99 compliant
-    #include "stdint.h"
-    #endif //_WIN32
+    typedef signed char int8_t;
+    typedef short int int16_t;
+    typedef int int32_t;
+    typedef long long int int64_t;
+    #endif //_WIN32 
 }
 
 #endif
