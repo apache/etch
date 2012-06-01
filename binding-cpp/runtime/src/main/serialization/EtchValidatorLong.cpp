d8bd1a2bcc8104bca0e3bcdda9b6f0e324dad087
Michael Fitzner <fitzner@apache.org>
2012-06-01 11:18:47 +0000

    ETCH-185 Implementation Validators
    Long Validator has been implemented
    
    Change-Id: Ic31b39b1219b77cafb644d2a5b6ad1cf971cb115
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/etch/trunk@1345062 13f79535-47bb-0310-9956-ffa450edef68

---
+++ binding-cpp/runtime/src/main/serialization/EtchValidatorLong.cpp
@@ -0,0 +1,114 @@
+/* $Id$
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "serialization/EtchValidatorLong.h"
+
+capu::SmartPointer<EtchValidator> EtchValidatorLong::mValidator[MAX_CACHED];
+const EtchObjectType EtchValidatorLong::TYPE(EOTID_VALIDATOR_LONG, NULL);
+
+EtchValidatorLong::EtchValidatorLong(capu::uint32_t ndim)
+: EtchTypeValidator(&EtchValidatorLong::TYPE, &EtchLong::TYPE, &EtchLong::TYPE, ndim) {
+
+}
+
+EtchValidatorLong::~EtchValidatorLong() {
+
+}
+
+capu::bool_t EtchValidatorLong::validate(EtchObject* value) {
+  if (value == NULL)
+    return false;
+
+  if (mExpectedType == NULL)
+    return false;
+
+  if (value->getObjectType()->equals(mExpectedType))
+    return true;
+
+  if ((value->getObjectType()->equals(&EtchInt32::TYPE)) ||
+          (value->getObjectType()->equals(&EtchShort::TYPE)) ||
+          (value->getObjectType()->equals(&EtchByte::TYPE)))
+    return true;
+
+  //handle array
+  if ((value->getObjectType()->isArray()) && (mExpectedType->isArray())) {
+
+    EtchNativeArray<EtchObject*> *array = (EtchNativeArray<EtchObject*> *) value;
+    const EtchObjectType* type = array->getContentType();
+    const EtchObjectType* type2 = mExpectedType->getObjectComponentType();
+    while ((type->getObjectComponentType() != NULL) && (mExpectedType->getObjectComponentType() != NULL)) {
+      type = type->getObjectComponentType();
+      type2 = type2->getObjectComponentType();
+    }
+
+    //both of them should be pointing null
+    if (type->getObjectComponentType() != type2->getObjectComponentType()) {
+      return false;
+    }
+    return type->equals(type2);
+  }
+}
+
+status_t EtchValidatorLong::validateValue(EtchObject* value, EtchObject*& result) {
+  if (validate(value)) {
+    if ((value->getObjectType()->equals(&EtchLong::TYPE)) || (mNDims > 0)) {
+      result = value;
+      return ETCH_OK;
+    } else {
+      if (value->getObjectType()->equals(&EtchShort::TYPE)) {
+        EtchShort *v = (EtchShort *) value;
+        result = new EtchLong((capu::int64_t)v->get());
+        return ETCH_OK;
+      }
+
+      if (value->getObjectType()->equals(&EtchByte::TYPE)) {
+        EtchByte *v = (EtchByte *) value;
+        result = new EtchLong((capu::int64_t)v->get());
+        return ETCH_OK;
+      }
+
+      if (value->getObjectType()->equals(&EtchInt32::TYPE)) {
+        EtchInt32 *v = (EtchInt32 *) value;
+        result = new EtchLong((capu::int64_t)v->get());
+        return ETCH_OK;
+      }
+      return ETCH_ERROR;
+    }
+  } else {
+    return ETCH_ERROR;
+  }
+}
+
+status_t EtchValidatorLong::Get(capu::uint32_t ndim, capu::SmartPointer<EtchValidator> &val) {
+  if (ndim > MAX_NDIMS) {
+    return ETCH_EINVAL;
+  }
+  if (ndim >= MAX_CACHED) {
+    val = new EtchValidatorLong(ndim);
+    return ETCH_OK;
+  }
+  if (mValidator[ndim].get() == NULL) {
+    mValidator[ndim] = new EtchValidatorLong(ndim);
+  }
+  val = mValidator[ndim];
+  return ETCH_OK;
+}
+
+status_t EtchValidatorLong::getElementValidator(capu::SmartPointer<EtchValidator> &val) {
+  return EtchValidatorLong::Get(mNDims - 1, val);
+}
---
+++ binding-cpp/runtime/src/test/serialization/EtchValidatorLongTest.cpp
@@ -0,0 +1,219 @@
+/* $Id$
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include "serialization/EtchValidatorLong.h"
+#include "capu/util/SmartPointer.h"
+#include "common/EtchString.h"
+
+TEST(EtchValidatorLongTest, createTest) {
+  capu::SmartPointer<EtchValidatorLong> ptr = NULL;
+
+  capu::SmartPointer<EtchValidator> val;
+  EXPECT_TRUE(EtchValidatorLong::Get(0, val) == ETCH_OK);
+  ptr = capu::smartpointer_cast<EtchValidatorLong>(val);
+
+  EXPECT_TRUE(ptr->getExpectedType()->equals(&EtchLong::TYPE));
+  EXPECT_TRUE(ptr->getNDims() == 0);
+
+  EtchObjectType type1(EOTID_LONG, NULL);
+  EtchObjectType type2(EOTID_NATIVE_ARRAY, &type1);
+  EtchObjectType type3(EOTID_NATIVE_ARRAY, &type2);
+  EXPECT_TRUE(EtchValidatorLong::Get(2, val) == ETCH_OK);
+  ptr = capu::smartpointer_cast<EtchValidatorLong>(val);
+  EXPECT_TRUE(ptr->getExpectedType()->equals(&type3));
+  EXPECT_TRUE(ptr->getNDims() == 2);
+
+}
+
+TEST(EtchValidatorLongTest, validateTest) {
+  EtchObject* byte = NULL;
+
+  EtchObject* integer = new EtchInt32(capu::NumericLimitMin<capu::int32_t>());
+  EtchObject* integer2 = new EtchInt32(0);
+  EtchObject* integer3 = new EtchInt32(capu::NumericLimitMax<capu::int32_t>());
+  EtchObject* integer4 = new EtchInt32(897);
+
+  EtchObject* longInteger = new EtchLong(capu::NumericLimitMin<capu::int32_t>());
+  EtchObject* longInteger2 = new EtchLong(0);
+  EtchObject* longInteger3 = new EtchLong(capu::NumericLimitMax<capu::int32_t>());
+  EtchObject* longInteger4 = new EtchLong(897);
+  //exceed limits of integer
+  EtchObject* longInteger5 = new EtchLong((capu::int64_t)capu::NumericLimitMax<capu::int32_t>() + (capu::int64_t)2);
+
+  EtchObject* shortInteger = new EtchShort(capu::NumericLimitMin<capu::int16_t>());
+  EtchObject* shortInteger2 = new EtchShort(0);
+  EtchObject* shortInteger3 = new EtchShort(capu::NumericLimitMax<capu::int16_t>());
+  EtchObject* shortInteger4 = new EtchShort();
+
+  //incompatible type
+  EtchObject* str = new EtchString();
+
+  EtchObject* byte1 = new EtchByte(capu::NumericLimitMax<capu::int8_t>());
+  EtchObject* byte2 = new EtchByte(0);
+  EtchObject* byte3 = new EtchByte(capu::NumericLimitMin<capu::int8_t>());
+  EtchObject* byte4 = new EtchByte(32);
+  capu::SmartPointer<EtchValidator> ptr = NULL;
+  EXPECT_TRUE(EtchValidatorLong::Get(0, ptr) == ETCH_OK);
+  EXPECT_TRUE((capu::smartpointer_cast<EtchTypeValidator>(ptr))->getNDims() == 0);
+  EXPECT_FALSE(ptr->validate(byte));
+  EXPECT_FALSE(ptr->validate(str));
+  EXPECT_TRUE(ptr->validate(longInteger5));
+  EXPECT_TRUE(ptr->validate(integer));
+  EXPECT_TRUE(ptr->validate(integer2));
+  EXPECT_TRUE(ptr->validate(integer3));
+  EXPECT_TRUE(ptr->validate(longInteger));
+  EXPECT_TRUE(ptr->validate(longInteger2));
+  EXPECT_TRUE(ptr->validate(longInteger3));
+  EXPECT_TRUE(ptr->validate(shortInteger));
+  EXPECT_TRUE(ptr->validate(shortInteger2));
+  EXPECT_TRUE(ptr->validate(shortInteger3));
+  EXPECT_TRUE(ptr->validate(byte1));
+  EXPECT_TRUE(ptr->validate(byte2));
+  EXPECT_TRUE(ptr->validate(byte3));
+  EXPECT_TRUE(ptr->validate(byte4));
+  EXPECT_TRUE(ptr->validate(integer4));
+  EXPECT_TRUE(ptr->validate(longInteger4));
+  EXPECT_TRUE(ptr->validate(shortInteger4));
+
+  delete integer;
+  delete integer2;
+  delete integer3;
+  delete integer4;
+
+  delete longInteger;
+  delete longInteger2;
+  delete longInteger3;
+  delete longInteger4;
+  delete longInteger5;
+
+  delete shortInteger;
+  delete shortInteger2;
+  delete shortInteger3;
+  delete shortInteger4;
+
+  delete str;
+  delete byte1;
+  delete byte2;
+  delete byte3;
+  delete byte4;
+
+}
+
+TEST(EtchValidatorLongTest, validateValueTest) {
+  EtchObject* byte = NULL;
+  EtchObject* result;
+  EtchObject* integer = new EtchInt32(-128);
+  EtchObject* integer2 = new EtchInt32(5);
+  EtchObject* integer3 = new EtchInt32(127);
+  EtchObject* byte2 = new EtchByte(3);
+  capu::SmartPointer<EtchValidator> ptr = NULL;
+  EXPECT_TRUE(EtchValidatorLong::Get(0, ptr) == ETCH_OK);
+  EXPECT_TRUE(ptr->validateValue(byte, result) == ETCH_ERROR);
+  EXPECT_TRUE(ptr->validateValue(integer, result) == ETCH_OK);
+  EXPECT_TRUE(((EtchLong*) result)->get() == ((EtchInt32*) integer)->get());
+  delete result;
+  EXPECT_TRUE(ptr->validateValue(integer2, result) == ETCH_OK);
+  EXPECT_TRUE(((EtchLong*) result)->get() == ((EtchInt32*) integer2)->get());
+  delete result;
+  EXPECT_TRUE(ptr->validateValue(integer3, result) == ETCH_OK);
+  EXPECT_TRUE(((EtchLong*) result)->get() == ((EtchInt32*) integer3)->get());
+  delete result;
+  EXPECT_TRUE(ptr->validateValue(byte2, result) == ETCH_OK);
+  EXPECT_TRUE(((EtchLong*) result)->get() == ((EtchByte*) byte2)->get());
+  delete result;
+  delete integer;
+  delete integer2;
+  delete integer3;
+  delete byte2;
+}
+
+TEST(EtchValidatorLongTest, elementValidatorTest) {
+  capu::SmartPointer<EtchValidator> ptr = NULL;
+  EXPECT_TRUE(EtchValidatorLong::Get(1, ptr) == ETCH_OK);
+  capu::SmartPointer<EtchValidator> elementValidator;
+  ptr->getElementValidator(elementValidator);
+
+  EtchObject* integer = new EtchInt32(capu::NumericLimitMin<capu::int32_t>());
+  EtchObject* integer2 = new EtchInt32(0);
+  EtchObject* integer3 = new EtchInt32(capu::NumericLimitMax<capu::int32_t>());
+  EtchObject* integer4 = new EtchInt32(897);
+
+  EtchObject* longInteger = new EtchLong(capu::NumericLimitMin<capu::int32_t>());
+  EtchObject* longInteger2 = new EtchLong(0);
+  EtchObject* longInteger3 = new EtchLong(capu::NumericLimitMax<capu::int32_t>());
+  EtchObject* longInteger4 = new EtchLong(897);
+  //exceed limits of integer
+  EtchObject* longInteger5 = new EtchLong((capu::int64_t)capu::NumericLimitMax<capu::int32_t>() + (capu::int64_t)2);
+
+  EtchObject* shortInteger = new EtchShort(capu::NumericLimitMin<capu::int16_t>());
+  EtchObject* shortInteger2 = new EtchShort(0);
+  EtchObject* shortInteger3 = new EtchShort(capu::NumericLimitMax<capu::int16_t>());
+  EtchObject* shortInteger4 = new EtchShort();
+
+  //incompatible type
+  EtchObject* str = new EtchString();
+
+  EtchObject* byte1 = new EtchByte(capu::NumericLimitMax<capu::int8_t>());
+  EtchObject* byte2 = new EtchByte(0);
+  EtchObject* byte3 = new EtchByte(capu::NumericLimitMin<capu::int8_t>());
+  EtchObject* byte4 = new EtchByte(32);
+
+  EXPECT_FALSE(elementValidator->validate(str));
+  EXPECT_TRUE(elementValidator->validate(longInteger5));
+  EXPECT_TRUE(elementValidator->validate(integer));
+  EXPECT_TRUE(elementValidator->validate(integer2));
+  EXPECT_TRUE(elementValidator->validate(integer3));
+  EXPECT_TRUE(elementValidator->validate(longInteger));
+  EXPECT_TRUE(elementValidator->validate(longInteger2));
+  EXPECT_TRUE(elementValidator->validate(longInteger3));
+  EXPECT_TRUE(elementValidator->validate(shortInteger));
+  EXPECT_TRUE(elementValidator->validate(shortInteger2));
+  EXPECT_TRUE(elementValidator->validate(shortInteger3));
+  EXPECT_TRUE(elementValidator->validate(byte1));
+  EXPECT_TRUE(elementValidator->validate(byte2));
+  EXPECT_TRUE(elementValidator->validate(byte3));
+  EXPECT_TRUE(elementValidator->validate(byte4));
+  EXPECT_TRUE(elementValidator->validate(integer4));
+  EXPECT_TRUE(elementValidator->validate(longInteger4));
+  EXPECT_TRUE(elementValidator->validate(shortInteger4));
+
+  delete integer;
+  delete integer2;
+  delete integer3;
+  delete integer4;
+
+  delete longInteger;
+  delete longInteger2;
+  delete longInteger3;
+  delete longInteger4;
+  delete longInteger5;
+
+  delete shortInteger;
+  delete shortInteger2;
+  delete shortInteger3;
+  delete shortInteger4;
+
+  delete str;
+  delete byte1;
+  delete byte2;
+  delete byte3;
+  delete byte4;
+}
+
+
