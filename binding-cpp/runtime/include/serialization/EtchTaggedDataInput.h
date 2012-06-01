8863e9a9bab369a61c3feaae4189c9a52d065af8
Martin Veith <veithm@apache.org>
2012-06-01 12:57:34 +0000

    ETCH-181 EtchTaggedData Interfaces
    Change-Id: Ida69e5a7e13b5b515ca0ca2165114b4e15e2dba1
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/etch/trunk@1345146 13f79535-47bb-0310-9956-ffa450edef68

---
+++ binding-cpp/runtime/include/serialization/EtchTaggedDataInput.h
@@ -0,0 +1,42 @@
+/* $Id$
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __ETCHTAGGEDDATAINPUT_H__
+#define	__ETCHTAGGEDDATAINPUT_H__
+#include "transport/EtchFlexBuffer.h"
+#include "transport/EtchMessage.h"
+#include "capu/util/SmartPointer.h"
+
+class EtchTaggedDataInput {
+public:
+
+  /**
+   * Reads a message from the buf.
+   * @param buf the flex buffer containing the message.
+   * @param a message read from the buf.
+   */
+  virtual status_t readMessage(capu::SmartPointer<EtchFlexBuffer> buf, EtchMessage *&message) = 0;
+
+  virtual ~EtchTaggedDataInput() {
+
+  }
+
+};
+
+#endif /* ETCHTAGGEDDATAINPUT_H */
+
---
+++ binding-cpp/runtime/include/serialization/EtchTaggedDataOutput.h
@@ -0,0 +1,43 @@
+/* $Id$
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
+#ifndef __ETCHTAGGEDDATAOUTPUT_H__
+#define __ETCHTAGGEDDATAOUTPUT_H__
+#include "transport/EtchFlexBuffer.h"
+#include "transport/EtchMessage.h"
+
+class EtchTaggedDataOutput {
+public:
+
+  /**
+   * Writes the message to the buf.
+   * @param msg the message to be written.
+   * @param buf the buffer to write to.
+   */
+  virtual status_t writeMessage(EtchMessage* msg, EtchFlexBuffer* buf) = 0;
+
+  virtual ~EtchTaggedDataOutput() {
+
+  }
+
+};
+
+
+#endif /* ETCHTAGGEDDATAOUTPUT_H */
+
