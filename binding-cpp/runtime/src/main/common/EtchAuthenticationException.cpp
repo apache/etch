a7a85373e78205c3ca6f67d2f3e19076fc19c720
Michael Fitzner <fitzner@apache.org>
2012-06-01 12:11:59 +0000

    ETCH-184 Implementation of EtchAuthenticationException
    EtchAuthenticationException has been implemented
    
    Change-Id: I5dac9912a4106376fe1fc4250f686b49746ff42e
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/etch/trunk@1345098 13f79535-47bb-0310-9956-ffa450edef68

---
+++ binding-cpp/runtime/src/main/common/EtchAuthenticationException.cpp
@@ -0,0 +1,44 @@
+/* $Id$
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "common/EtchAuthenticationException.h"
+
+const EtchObjectType* EtchAuthenticationException::TYPE() {
+  const static EtchObjectType TYPE(EOTID_AUTH_EXCEPTION, NULL);
+  return &TYPE;
+}
+
+EtchAuthenticationException::EtchAuthenticationException(EtchString& msg)
+: EtchException(msg, ETCH_ERROR, EXCPTYPE_BUILTIN, EtchAuthenticationException::TYPE()) {
+
+}
+
+EtchAuthenticationException::~EtchAuthenticationException() {
+
+}
+
+capu::bool_t EtchAuthenticationException::equals(const EtchObject* other) {
+  if (other == NULL)
+    return false;
+  else if (other->getObjectType() != EtchAuthenticationException::TYPE())
+    return false;
+  EtchAuthenticationException * ptr = (EtchAuthenticationException*) other;
+  return ((ptr->mErrorcode == this->mErrorcode)
+          && (ptr->mExcptype == this->mExcptype)
+          && (ptr->mErrorMessage.equals(&this->mErrorMessage)));
+}
+
---
+++ binding-cpp/runtime/src/test/common/EtchAuthenticationExceptionTest.cpp
@@ -0,0 +1,61 @@
+/* $Id$
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include "common/EtchAuthenticationException.h"
+
+TEST(EtchAuthenticationExceptionTest, createTest) {
+  EtchString str("test message");
+  EtchAuthenticationException *test = NULL;
+  test = new EtchAuthenticationException(str);
+  EXPECT_TRUE(test != NULL);
+  delete test;
+}
+
+TEST(EtchAuthenticationExceptionTest, getTests) {
+  EtchString str("test message");
+  EtchAuthenticationException *test = NULL;
+  test = new EtchAuthenticationException(str);
+  EXPECT_TRUE(test != NULL);
+  EXPECT_TRUE(test->getErrorMessage().equals(&str));
+  EXPECT_TRUE(test->getErrorCode() == ETCH_ERROR);
+  EXPECT_TRUE(test->getExceptionType() == EtchException::EXCPTYPE_BUILTIN);
+  delete test;
+}
+
+TEST(EtchAuthenticationExceptionTest, equalsTest) {
+  EtchString str("test message");
+  EtchString str2("test2 message");
+  EtchAuthenticationException *test = NULL;
+  test = new EtchAuthenticationException(str);
+  EtchAuthenticationException *test2 = NULL;
+  test2 = new EtchAuthenticationException(str2);
+  EtchAuthenticationException *test3 = NULL;
+  test3 = new EtchAuthenticationException(str);
+
+  EXPECT_TRUE(test != NULL);
+  EXPECT_TRUE(test2 != NULL);
+  EXPECT_TRUE(test3 != NULL);
+
+  EXPECT_TRUE(test->equals(test3));
+  EXPECT_FALSE(test->equals(test2));
+
+  delete test;
+  delete test2;
+  delete test3;
+}
