efcdc9320848f29e74c90a48e01c1148f988423a
Michael Fitzner <fitzner@apache.org>
2012-08-02 13:51:29 +0000

    ETCH-141: EtchPool Interface
    Added Interface for EtchPool
    Added Interface for EtchPoolRunnable
    
    Change-Id: Iefc2950e2b98e9063e268a6342cc5f0af39cf553
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/etch/trunk@1368474 13f79535-47bb-0310-9956-ffa450edef68

---
+++ binding-cpp/runtime/include/support/EtchPoolRunnable.h
@@ -0,0 +1,45 @@
+/* $Id$
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __ETCHPOOLRUNNABLE_H__
+#define __ETCHPOOLRUNNABLE_H__
+
+#include "capu/util/SmartPointer.h"
+#include "common/EtchException.h"
+
+/**
+ *  A runnable which can throw an exception.
+ */
+class EtchPoolRunnable
+{
+public:
+
+   /**
+   * Execute the runnable
+   * @return error core if some exeception occures
+   */
+  virtual capu::status_t run() = 0;
+
+  /**
+   * Reports an exception thrown by the run method.
+   * @param e exception
+   */
+  virtual capu::status_t exception(capu::SmartPointer<EtchException> exception) = 0;
+};
+
+#endif
