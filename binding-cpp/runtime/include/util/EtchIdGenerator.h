e9852c78478ad63034d7b0a65add05139f232d20
Michael Fitzner <fitzner@apache.org>
2012-08-02 14:56:38 +0000

    ETCH-149: EtchIdGenerator Implementation
    Change-Id: I4cb22b3e6d7e1e655d10323e505415d5c67d9a30
    
    git-svn-id: https://svn.apache.org/repos/asf/incubator/etch/trunk@1368502 13f79535-47bb-0310-9956-ffa450edef68

---
+++ binding-cpp/runtime/include/util/EtchIdGenerator.h
@@ -0,0 +1,71 @@
+/* $Id$
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef __ETCHIDGENERATOR_H__
+#define __ETCHIDGENERATOR_H__
+
+#include "capu/os/Mutex.h"
+#include "common/EtchConfig.h"
+
+/**
+ * Synchronously generates long id numbers.
+ */
+class EtchIdGenerator {
+public:
+
+  /**
+   * Constructs the IdGenerator with the default starting point of 1
+   * and the default stride of 1.
+   */
+  EtchIdGenerator();
+
+  /**
+   * Constructs the IdGenerator with the specified starting point
+   * and the default stride of 1.
+   *
+   * @param nextId
+   */
+  EtchIdGenerator(capu::int64_t nextId);
+
+  /**
+   * Constructs the IdGenerator with the specified starting point
+   * and the specified stride;
+   * @param nextId
+   * @param stride
+   */
+  EtchIdGenerator(capu::int64_t nextId, capu::uint32_t stride);
+
+  /**
+   * Destructor
+   */
+  virtual ~EtchIdGenerator();
+
+  /**
+   * @return the next id in sequence.
+   */
+  capu::int64_t next();
+
+private:
+
+  capu::int64_t mNextId;
+  const capu::uint32_t mStride;
+  capu::Mutex mMutex;
+};
+
+#endif	/* ETCHIDGENERATOR_H */
+
---
+++ binding-cpp/runtime/src/main/util/EtchIdGenerator.cpp
@@ -0,0 +1,44 @@
+/* $Id$
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements. See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to you under the Apache License, Version
+ * 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#include "util/EtchIdGenerator.h"
+#include "capu/os/Mutex.h"
+
+//TODO: Replace this via AtomicOperations
+
+EtchIdGenerator::~EtchIdGenerator() {
+}
+
+EtchIdGenerator::EtchIdGenerator()
+: mNextId(1), mStride(1) {
+}
+
+EtchIdGenerator::EtchIdGenerator(capu::int64_t nextId)
+: mNextId(nextId), mStride(1) {
+}
+
+EtchIdGenerator::EtchIdGenerator(capu::int64_t nextId, capu::uint32_t stride)
+: mNextId(nextId), mStride(stride) {
+}
+
+capu::int64_t EtchIdGenerator::next() {
+  mMutex.lock();
+  capu::int64_t id = mNextId;
+  mNextId += mStride;
+  mMutex.unlock();
+  return id;
+}
